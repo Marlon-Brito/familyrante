3e05b0e9fa02346ceba92b12938376b4fe9bd800
Marlon Brito <blackreaper4399@gmail.com>
2025-01-17 18:38:01 -0300

    Enviando projeto para deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
-// Importando imagens
-import Fundo from '../public/assets/images/home_background.jpg';
+// Importando imagens e videos
 import Sobre from '../public/assets/images/sobre.jpg';
+import Video from '../public/assets/videos/restaurante.mp4';
 // Importando styles e scripts
 import '../public/assets/css/index.css';
 import '../public/assets/css/cardapio.css';
@@ -17,28 +17,31 @@ function App() {
   // Definindo lista de páginas do menu
   const paginaMenu = [pratosPrincipais, sobremesas, bebidas]
   // Monitorando qual página está selecionada com state
-  const [paginaSelecionada, atualizarPaginaSelecionada] = useState(0);
+  const [paginaSelecionada, atualizarPaginaSelecionada] = useState(0); 
+
 
   useEffect(() => {
     // Necessita instanciar e passar uma função com parâmetro
-    const myObserver = new IntersectionObserver((entries) => {
+    const observer = new IntersectionObserver((entries, observerInstance) => {
       // Agora se receberá todas entradas, então deve-se separar estas listas de dados
       entries.forEach((entry) => {
-        // Aí verá cada entrada, se ela estiver na tela se aplicará a classe show senão continuará hidden
-        if (entry.isIntersecting){
+        // Verá cada entrada, se ela estiver na tela se aplicará a classe 
+        if (entry.isIntersecting) {
+          // Adicionar a classe "show" para torná-la visível
           entry.target.classList.add('show');
-        }else{
-          entry.target.classList.remove('show');
+          // Parar de observar essa seção
+          observerInstance.unobserve(entry.target);
         }
-      })
+      });
     });
 
-    // Selecionando todos elementos escondidos para observar e mostrar
-    const elements = document.querySelectorAll('.hidden');
+    // Selecionar todas as seções com a classe "hidden"
+    const hiddenElements = document.querySelectorAll('.hidden');
+    hiddenElements.forEach((element) => observer.observe(element));
 
-    // Pra funcionar ele vai observar todos elementos e acessará suas informações vendo quando são visíveis ou não, assim percorrerá e observará cada elemento
-    elements.forEach((element) => myObserver.observe(element));
-  });
+    // Cleanup (limpeza) para evitar memory leaks (vazamentos de memória)
+    return () => observer.disconnect();
+  }, []);
 
   return (
     <>
@@ -47,18 +50,22 @@ function App() {
 
       {/* Conteúdo Principal */}
       <main className='conteudo--principal'>
-        <div className="container__fundo">
-          <img src={Fundo} alt="Fundo de Entrada" className='img__fundo'/>
+        <video autoPlay loop muted>
+          <source src={Video}  type="video/mp4" />
+        </video>
+
+        <div className="apresentacao">
           <h1 className='titulo'>Seja bem-vindo ao <span className="titulo--destaque">Familyrante</span>!</h1>
+          <p className='slogan'>O melhor restaurante! Onde você come em família.</p>
         </div>
         
-        <section className='hidden'>
+        <section className='hidden' id='cardapio'>
           {/* Cardápio: menu de navegação e itens do cardápio */}
           <h2 className='subtitulo container'>Cardápio</h2>
 
           <Navegacao atualizarPaginaSelecionada={atualizarPaginaSelecionada}/>
 
-          <ul className='menu__cardapio'>
+          <ul className='menu__cardapio container'>
             {
               paginaMenu[paginaSelecionada].map(item => <ItemCardapio 
                 nome={item.nome} 
@@ -72,27 +79,27 @@ function App() {
 
         {/* Seções de Informações: Sobre, Serviços e Contato */}
         <section className='sobre hidden' id='sobre'>
-          <h2 className='container'>Sobre</h2>
-          <p>Família, restaurante e comida. Venha conhecer o Familyrante, o restaurante que busca trazer aquele gostinho caseiro e aconchegante de poder apreciar uma comida junto da família, à comida bem feita e de qualidade para saciar a fome.</p>
+          <h2 className='subtitulo container'>Sobre</h2>
+          <p>Família, restaurante e comida. Venha conhecer o Familyrante, o restaurante que busca trazer aquele gostinho caseiro e aconchegante de poder apreciar uma comida junto da família, comida bem feita e de qualidade para saciar a fome.</p>
 
           <img src={Sobre} alt="Sobre"/>
         </section>
 
         <section className='funcionamento hidden' id='funcionamento'>
-          <h2 className='container'>Serviços</h2>
-          <h3>Horário de Funcionamento</h3>
-          <ul>
-            <li>Sábado: Fechado</li>
+          <h2 className='subtitulo container'>Serviços</h2>
+          <h3 className='subtitulo__horario'>Horário de Funcionamento</h3>
+          <ul className='lista__horario'>
+            <li>Domingo: Fechado</li>
             <li>Segunda-feira: 08 às 20:00</li>
             <li>Terça-feira: 08 às 20:00</li>
             <li>Quarta-feira: 08 às 20:00</li>
             <li>Quinta-feira: 08 às 20:00</li>
             <li>Sexta-feira: 08 às 20:00</li>
-            <li>Domingo: Fechado</li>
+            <li>Sábado: Fechado</li>
           </ul>
 
-          <h3>Formas de Pagamento</h3>
-          <ul>
+          <h3 className='subtitulo__pagamento'>Formas de Pagamento</h3>
+          <ul className='lista__pagamento'>
             <li>Dinheiro</li>
             <li>Pix</li>
             <li>Cartão de Crédito</li>
@@ -102,7 +109,7 @@ function App() {
         </section>
 
         <section className='contato hidden' id='contato'>
-          <h2 className='container'>Contato</h2>
+          <h2 className='subtitulo container'>Contato</h2>
 
           <ul>
             <li><a href="#"><i class="bi bi-instagram icones" id='insta'></i> Instagram</a></li>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,20 @@
-import Logo from '../../public/assets/images/logo-p.png';
+import Logo from '../../public/assets/images/logo-familyrante-g.png';
 import '../../public/assets/css/header.css';
 
 export default function Header(){
     return(
         <header className='cabecalho'>
             <nav className='menu'>
-            <a href="./index.html">
-                <img src={Logo} alt='Logo Familyrante' className='logo'/>
-            </a>
+                <a href="./index.html">
+                    <img src={Logo} alt='Logo Familyrante' className='logo'/>
+                </a>
 
-            <ul>
-            <a href="#sobre"><li>Sobre</li></a>
-            <a href="#funcionamento"><li>Serviços</li></a>
-            <a href="#contato"><li>Contato</li></a>
-            </ul>
+                <ul>
+                    <a href="#cardapio"><li>Cardápio</li></a>
+                    <a href="#sobre"><li>Sobre</li></a>
+                    <a href="#funcionamento"><li>Serviços</li></a>
+                    <a href="#contato"><li>Contato</li></a>
+                </ul>
             </nav>
         </header>
     )
