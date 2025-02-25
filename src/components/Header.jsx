091613687768937cd6951383b49c9502afe65dd9
Marlon Brito <blackreaper4399@gmail.com>
2025-02-25 00:05:37 -0300

    Atualizações finais

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,38 +1,48 @@
 import { useEffect, useState } from "react";
-import Logo from '../../public/assets/images/logo-familyrante.png';
+import Logo from "../../public/assets/images/logo-familyrante.png";
 
-export default function Header(){
-    // Monitorando evento de rolagem para adicionar outro estilo ao cabeçalho
-    const [scrolled, setScrolled] = useState(false);
+export default function Header() {
+  // Monitorando evento de rolagem para adicionar outro estilo ao cabeçalho
+  const [scrolled, setScrolled] = useState(false);
 
-    useEffect(() => {
-        const handleScroll = () => {
-            if (window.scrollY > 50) {
-                setScrolled(true);
-            } else {
-                setScrolled(false);
-            }
-        };
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
 
-        window.addEventListener("scroll", handleScroll);
-        return () => {
-            window.removeEventListener("scroll", handleScroll);
-        };
-    }, []);
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
-    return(
-        <header className={`cabecalho ${scrolled ? "scrolled" : ""}`}>
-            <img src={Logo} alt="Logo Familyrante" className='logo'/>
+  return (
+    <header className={`cabecalho ${scrolled ? "scrolled" : ""}`}>
+      <img src={Logo} alt="Logo Familyrante" className="logo" />
 
-            <nav>
-                <ul className="menu">
-                    <li><a href="#inicio">Início</a></li>
-                    <li><a href="#cardapio">Cardápio</a></li>
-                    <li><a href="#sobre">Sobre</a></li>
-                    <li><a href="#servicos">Serviços</a></li>
-                    <li><a href="#contato">Contato</a></li>
-                </ul>
-            </nav>
-        </header>
-    )
+      <nav>
+        <ul className="menu">
+          <li>
+            <a href="#inicio">Início</a>
+          </li>
+          <li>
+            <a href="#cardapio">Cardápio</a>
+          </li>
+          <li>
+            <a href="#sobre">Sobre</a>
+          </li>
+          <li>
+            <a href="#servicos">Serviços</a>
+          </li>
+          <li>
+            <a href="#contato">Contato</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
