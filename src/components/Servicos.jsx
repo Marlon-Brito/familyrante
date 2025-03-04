63045e3753af0397e3f67a4078561d1c4d6da65f
Marlon Brito <blackreaper4399@gmail.com>
2025-03-04 17:08:09 -0300

    Separando partials scss

--- src/components/Servicos.jsx
+++ src/components/Servicos.jsx
@@ -9,22 +9,22 @@ export default function Servicos(){
                     <h2 className='subtitulo'>Serviços</h2>
                     <h3 className='subtitulo__horario'>Horário de Funcionamento</h3>
                     <ul className='lista__horario'>
-                    <li>Domingo: Fechado</li>
-                    <li>Segunda-feira: 08 às 20:00</li>
-                    <li>Terça-feira: 08 às 20:00</li>
-                    <li>Quarta-feira: 08 às 20:00</li>
-                    <li>Quinta-feira: 08 às 20:00</li>
-                    <li>Sexta-feira: 08 às 20:00</li>
-                    <li>Sábado: Fechado</li>
+                        <li>Domingo: Fechado</li>
+                        <li>Segunda-feira: 08 às 20:00</li>
+                        <li>Terça-feira: 08 às 20:00</li>
+                        <li>Quarta-feira: 08 às 20:00</li>
+                        <li>Quinta-feira: 08 às 20:00</li>
+                        <li>Sexta-feira: 08 às 20:00</li>
+                        <li>Sábado: Fechado</li>
                     </ul>
 
                     <h3 className='subtitulo__pagamento'>Formas de Pagamento</h3>
                     <ul className='lista__pagamento'>
-                    <li>Dinheiro</li>
-                    <li>Pix</li>
-                    <li>Cartão de Crédito</li>
-                    <li>Cartão de Débito</li>
-                    <li><small>( Aceitamos todas as bandeiras )</small></li>
+                        <li>Dinheiro</li>
+                        <li>Pix</li>
+                        <li>Cartão de Crédito</li>
+                        <li>Cartão de Débito</li>
+                        <li><small>( Aceitamos todas as bandeiras )</small></li>
                     </ul>
                 </div>
 
